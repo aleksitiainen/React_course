ff9b1965ba5c1675e72f0529bfaa58e2fc556313
Teemu kivimäki <ninojaallu@gmail.com>
2025-12-18 10:39:13 +0200

    Phonenumber deletion added

--- osa-2/puhelinluottelo_2.11/puhelinluottelo/src/App.jsx
+++ osa-2/puhelinluottelo_2.11/puhelinluottelo/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
 import json_server from './service.js'
+import Notification from './Notification.jsx'
+import './App.css'
 
 const Input = ({ label, state, setState }) => {
   return (
@@ -11,28 +13,33 @@ const Input = ({ label, state, setState }) => {
   )
 }
 
-const Form = ({ state, setState }) => {
+const Form = ({ state, setState, errorState, successState }) => {
   const [newName, setNewName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault();
 
-    if (!newName) return alert('Name cant be empty');
+    if (!newName) {
+      console.error('Name cant be empty');
+      return errorState('Name cant be empty');
+    } 
 
     for (const person of state) {
       if (person.name == newName) {
-        return alert(`Name ${newName} is already in phonebook`);
+        console.error(`Name ${newName} is already in phonebook`);
+        return errorState(`Name ${newName} is already in phonebook`);
       }
     }
 
     json_server
     .create({ name: newName, number: phoneNumber, id: String(Number(state[state.length - 1]?.id) + 1) })
     .then(response => {
-      console.log(response)
+      errorState('')
       setState(prev => prev.concat(response.data))
       setNewName('')
-      setPhoneNumber('')
+      setPhoneNumber('');
+      successState(`${newName} added to phonebook`)
     })
   }
 
@@ -60,7 +67,9 @@ const Part = ({ content }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  
   useEffect(() => {
     json_server.getAll()
     .then(r => {      
@@ -71,19 +80,33 @@ const App = () => {
   const [filter, setFilter] = useState('')
   const filteredPersons = Filter(persons, filter)
 
+  const DeletePerson = (id) => {
+    json_server.delete(id)
+    .then(r => {
+      setPersons(prev => prev.filter(p => p.id !== r.data.id))
+    })
+  }
+
   return (
     <div>
       <h1>Phonebook</h1>
       <Input label={"Filter"} state={filter} setState={setFilter}/>
+      
       <h2>Add new one</h2>
-      <Form state={persons} setState={setPersons}/>
+      {errorMessage && <Notification className={"error"} message={errorMessage} /> }
+      {successMessage && <Notification className={"success"} message={successMessage} /> }
+      <Form state={persons} setState={setPersons} errorState={setErrorMessage} successState={setSuccessMessage}/>
+
       <h2>Numbers</h2>
       {filteredPersons.map((p, idx) => 
-        <table key={idx}>
+        <table key={p.id}>
           <tbody>
             <tr>
               <Part content={p.name} />
               <Part content={p.number} />
+              <td>
+                <button onClick={() => DeletePerson(p.id)}>Delete</button>
+              </td>
             </tr>
           </tbody>
         </table>
---
+++ osa-2/puhelinluottelo_2.11/puhelinluottelo/src/Notification.jsx
@@ -0,0 +1,16 @@
+const Notification = ({ message, className }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div >
+        <p className={className}>
+            {message}
+
+        </p>
+    </div>
+  )
+}
+
+export default Notification
